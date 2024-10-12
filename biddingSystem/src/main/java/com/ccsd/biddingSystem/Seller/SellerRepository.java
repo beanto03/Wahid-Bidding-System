6dfff0382f4707e725fd81fbc5433779a32b2dd3
User <aunisyafiqah139@gmail.com>
2024-10-13 02:04:19 +0800

    Register and login update

--- biddingSystem/src/main/java/com/ccsd/biddingSystem/Seller/SellerRepository.java
+++ biddingSystem/src/main/java/com/ccsd/biddingSystem/Seller/SellerRepository.java
@@ -1,7 +1,9 @@
 package com.ccsd.biddingSystem.Seller;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SellerRepository extends MongoRepository<Seller, String> {
-    Seller findByEmail(String email);  // Method to find a buyer by email
+    Seller findByEmail(String email);
 }
